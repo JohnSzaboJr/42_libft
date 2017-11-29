026573c367607f2f0e852e7672b7e3a5d50323b9
John Szabo <john.szabo.jr@gmail.com>
2017-11-29 13:44:56 +0100

    most list functions done

---
+++ ft_lstdel.c
@@ -0,0 +1,29 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jszabo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/14 10:32:00 by jszabo            #+#    #+#             */
+/*   Updated: 2017/11/21 10:42:44 by jszabo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdel(t_list **alst, void (*del)(void*, size_t))
+{
+	t_list *list;
+
+	list = *alst;
+	while (list)
+	  {
+	del(list->content, list->content_size);
+	free(list);
+	list = list->next;
+	  }
+	*alst = NULL;
+}
---
+++ ft_lstdelone.c
@@ -0,0 +1,25 @@
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jszabo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/14 10:32:00 by jszabo            #+#    #+#             */
+/*   Updated: 2017/11/21 10:42:44 by jszabo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t))
+{
+	t_list *list;
+
+	list = *alst;
+  	del(list->content, list->content_size);
+	free(list);
+	*alst = NULL;
+}
